e4099536b2983489fc584909efbd7a23057c34a4
Oscar Daniel <92224083+idangerl@users.noreply.github.com>
2022-11-14 12:22:02 -0400

    ejercicio sets y objetos

---
+++ conjuntos.js
@@ -0,0 +1,10 @@
+const array = ['Oscar','Daniel','Antonia','Fernando','Maria']
+const nombresSet = new Set(array)
+console.log(nombresSet)
+
+nombresSet.add('Oscar')
+console.log(nombresSet)
+
+nombresSet.add('JavaScript')
+console.log(nombresSet)
+
